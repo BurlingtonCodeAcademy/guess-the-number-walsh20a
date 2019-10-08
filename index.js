a838f23129977ece8c8f58669b815e4efabd937f
walsh20a <walsh20a@gmail.com>
2019-10-08 19:35:02 -0400

    BECAUSE I'M DONE.

--- index.js
+++ index.js
@@ -9,10 +9,27 @@ function ask(questionText) {
 
 start();
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 async function start() {
   console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
   let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
   console.log('You entered: ' + secretNumber);
-  // Now try and complete the program.
+
+let guess = getRandomInt(100) 
+let userResponse = await ask("Is " + guess + " your number?")
+
+
+while (check = false){let guess = getRandomInt(100) 
+  let userResponse = await ask("Is " + guess + " your number?")}
+  
+  let check = if (guess === secretNumber) { console.log("I guessed it! I'm correct! Computer Wins.") }
+
+  else if (guess > secretNumber) { console.log("Guess Lower.") }
+
+
+
   process.exit();
 }
